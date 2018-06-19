33840416e046b42657f34999b67b120356b563c0
Lucas Whitman <lwhitman@levelsbeyond.com>
2018-06-19 07:13:44 -0600

    partial commit, posts and categories

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def food
+    posts_for_branch(params[:action])
+  end
+
+  def hobby
+    posts_for_branch(params[:action])
+  end
+
+  private
+
+  def get_posts
+    Post.limit(30)
+  end
+  
+  def posts_for_branch(branch)
+    @categories = Category.where(branch: branch)
+    @posts = get_posts.paginate(page: params[:page])
+  end
 end
